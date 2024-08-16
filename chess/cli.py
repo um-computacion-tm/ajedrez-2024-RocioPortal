dde55ad648ed3deab8aa4ffb5b9b0f2edfe80bbb
RocioPortal <p.portal@alumno.um.edu.ar>
2024-08-15 23:07:27 -0300

    validar que las coordenadas esten dentro del rango permitido

--- chess/cli.py
+++ chess/cli.py
@@ -19,7 +19,15 @@ def play (chess):
             to_row,
             to_col
         )
-
+        
+        # Validar que las coordenadas están dentro del rango permitido
+            
+        if not (0 <= from_row < 8) or not (0 <= from_col < 8):
+                raise ValueError("Las coordenadas de inicio están fuera del rango permitido.")
+        if not (0 <= to_row < 8) or not (0 <= to_col < 8):
+                raise ValueError("Las coordenadas de destino están fuera del rango permitido.")
+            
+            
     except Exception as e:
         print("error")
 
