96c99897e3cdeb49aeff79dcd7a7aa327e43b572
RocioPortal <p.portal@alumno.um.edu.ar>
2024-08-18 20:55:39 -0300

    intentando agregar el movimiento horizontal de la torre

--- chess/rook.py
+++ chess/rook.py
@@ -6,3 +6,9 @@ class Rook(Piece):
         #estan cambiando, lo que indica un movimiento diagonal
         if from_row != to_row and from_col != to_col:
             return False
+        
+        if from_row == to_row:  # Movimiento horizontal
+            step = 1 if to_col > from_col else -1
+            for col in range(from_col + step, to_col, step):
+                if board.get_piece(from_row, col) is not None:
+                    return False  #si hay una pieza bloqueando el camino devuelve false 
