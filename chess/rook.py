e0d82099725d943f63fbb9a050731e7c26dd8c77
RocioPortal <p.portal@alumno.um.edu.ar>
2024-08-17 22:04:42 -0300

    Agregando condicion que evalúa si la torre se mueve en diagonal

--- chess/rook.py
+++ chess/rook.py
@@ -1,4 +1,8 @@
 from pieces import Piece
 
 class Rook(Piece):
-    ...    
+    def is_valid_move(self, from_row, from_col, to_row, to_col, board):
+        # Verifica si el movimiento es válido para una torre ya que si ambos son verdaderos significa que la fila y la columna 
+        #estan cambiando, lo que indica un movimiento diagonal
+        if from_row != to_row and from_col != to_col:
+            return False
