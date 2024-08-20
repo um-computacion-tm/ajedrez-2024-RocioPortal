e423f91d2e64ab4edc88860127840b5f3098e5ac
RocioPortal <p.portal@alumno.um.edu.ar>
2024-08-19 21:33:18 -0300

    Intentando agregar el movimiento vertical de la torre

--- chess/rook.py
+++ chess/rook.py
@@ -12,3 +12,12 @@ class Rook(Piece):
             for col in range(from_col + step, to_col, step):
                 if board.get_piece(from_row, col) is not None:
                     return False  #si hay una pieza bloqueando el camino devuelve false 
+
+
+        if from_col == to_col:  # Movimiento vertical
+            step = 1 if to_row > from_row else -1
+            for row in range(from_row + step, to_row, step):
+                if board.get_piece(row, from_col) is not None:
+                    return False
+
+        return True
