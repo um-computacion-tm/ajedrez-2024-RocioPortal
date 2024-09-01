c19f2a5caea2bd04006d21c4941db262648b3be0
RocioPortal <p.portal@alumno.um.edu.ar>
2024-08-31 23:11:49 -0300

    testeando funciones del board

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,7 @@
 import unittest
 from chess.board import Board
+from chess.rook import Rook
+from chess.pawn import Pawn
 
 
 class TestBoard(unittest.TestCase):
@@ -22,3 +24,24 @@ class TestBoard(unittest.TestCase):
 
     def setUp(self):
         self.board = Board()
+
+    def test_initial_positions(self):
+        # Verificar que las piezas están en las posiciones correctas
+        self.assertIsInstance(self.board.get_piece(0, 0), Rook)
+        self.assertEqual(self.board.get_piece(0, 0).get_color, "BLACK")
+        self.assertIsInstance(self.board.get_piece(7, 0), Rook)
+        self.assertEqual(self.board.get_piece(7, 0).get_color, "WHITE")
+        self.assertIsInstance(self.board.get_piece(1, 0), Pawn)
+        self.assertEqual(self.board.get_piece(1, 0).get_color, "BLACK")
+        self.assertIsInstance(self.board.get_piece(6, 0), Pawn)
+        self.assertEqual(self.board.get_piece(6, 0).get_color, "WHITE")
+
+    def test_get_piece(self):
+        # Verificar que get_piece devuelve la pieza correcta
+        rook = self.board.get_piece(0, 0)
+        self.assertIsInstance(rook, Rook)
+        self.assertEqual(rook.get_color, "BLACK")
+
+        pawn = self.board.get_piece(6, 0)
+        self.assertIsInstance(pawn, Pawn)
+        self.assertEqual(pawn.get_color, "WHITE")
