6c3b5068818905732007eb2f5a45da2044b1d8ef
RocioPortal <p.portal@alumno.um.edu.ar>
2024-09-14 13:40:36 -0300

    movimientos del peón

--- chess/pawn.py
+++ chess/pawn.py
@@ -8,6 +8,57 @@ class Pawn(Piece):
       else:
          return "♙" 
     
+
+    def get_possible_positions(self, from_row, from_col):
+        possibles = self.get_possible_positions_move(
+            from_row,
+            from_col,
+        )
+        possibles.extend(
+            self.get_possible_positions_eat(from_row, from_col)
+        )
+        return possibles
+
+    def get_possible_positions_eat(self, from_row, from_col):
+        if self.__color__ == "BLACK":
+            other_piece = self.__board__.get_piece(from_row + 1, from_col + 1)
+            if other_piece and other_piece.__color__ == "WHITE":
+                return [(from_row + 1, from_col + 1)]
+
+        return []
+
+    def get_possible_positions_move(self, from_row, from_col):
+        if self.__color__ == "BLACK":
+            if self.__board__.get_piece(from_row + 1, from_col) is None:
+                if (
+                    from_row == 1 and
+                    self.__board__.get_piece(from_row + 2, from_col) is None
+                ):
+                    return [
+                        (from_row + 1, from_col),
+                        (from_row + 2, from_col)
+                    ]
+                else:
+                    return [
+                        (from_row + 1, from_col),
+                    ]
+        else:
+            if from_row == 6:
+                return [
+                    (from_row - 1, from_col),
+                    (from_row - 2, from_col)
+                ]
+            else:
+                if self.__board__.get_piece(from_row - 1, from_col) is None:
+                    return [
+                        (from_row - 1, from_col),
+                    ]
+                else:
+                    return []
+        return []
+
+
+    '''
     def is_valid_move(self, board, from_row, from_col, to_row, to_col):
         # Movimiento vertical básico
         direction = 1 if self.color == "WHITE" else -1
@@ -30,3 +81,5 @@ class Pawn(Piece):
                 return True
         
         return False
+
+    '''
