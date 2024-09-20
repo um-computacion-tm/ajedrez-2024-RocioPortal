8d2f3d26ffd4a88b1cc5488f4c0d609cdb4f55fe
RocioPortal <p.portal@alumno.um.edu.ar>
2024-09-20 20:38:24 -0300

    agregando los movimientos del rey

--- chess/king.py
+++ chess/king.py
@@ -8,3 +8,12 @@ class King(Piece):
           return "♚"
       else:
          return "♔" 
+      
+    def valid_positions(self, from_row, from_col, to_row, to_col):
+        # El rey puede moverse una casilla en cualquier dirección (ortogonal o diagonal)
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1),   # Movimientos ortogonales
+                      (1, 1), (-1, -1), (1, -1), (-1, 1)] # Movimientos diagonales
+                      
+        # single_step=True porque el rey solo se mueve una casilla en cualquier dirección
+        possible_positions = self.possible_moves_general(from_row, from_col, directions, single_step=True)
+        return (to_row, to_col) in possible_positions
