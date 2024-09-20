be4678186b1370a6fa147a2dd3b8e59b7e06aa06
RocioPortal <p.portal@alumno.um.edu.ar>
2024-09-20 20:39:32 -0300

    agregando los movimientos de la reina

--- chess/queen.py
+++ chess/queen.py
@@ -8,3 +8,11 @@ class Queen(Piece):
           return "♛"
       else:
          return "♕"    
+      
+
+    def valid_positions(self, from_row, from_col, to_row, to_col):
+        # Movimientos ortogonales y diagonales
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1),   # Ortogonales
+                      (1, 1), (-1, -1), (1, -1), (-1, 1)] # Diagonales
+        possible_positions = self.possible_moves_general(from_row, from_col, directions, single_step = False)
+        return (to_row, to_col) in possible_positions
