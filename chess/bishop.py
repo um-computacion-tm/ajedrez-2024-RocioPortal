0dbb261331aa3fda628a1c0191c5a6d608b374ba
RocioPortal <p.portal@alumno.um.edu.ar>
2024-09-20 20:43:16 -0300

    movimientos del alfil adaptados

--- chess/bishop.py
+++ chess/bishop.py
@@ -4,15 +4,12 @@ class Bishop(Piece):
     white_str = "♝"  
     black_str = "♗"
 
-    def valid_positions(self, from_row, from_col, to_col, to_row):
-      
-      possible_diagonal_positions = (
-         self.possible_positions_dai(from_row, from_col)+ #Diagonal ascendente izquierda
-         self.possible_positions_dad(from_row, from_col)+ #Diagonal ascendente derecha
-         self.possible_positions_ddi(from_row, from_col)+ #Diagonal descendente izquierda
-         self.possible_positions_ddd(from_row, from_col)  #Diagonal desscendente derecha
-      )
-      return (to_row, to_col) in possible_diagonal_positions
+    def valid_positions(self, from_row, from_col, to_row, to_col):
+        # Movimientos diagonales
+        directions = [(1, 1), (-1, -1), (1, -1), (-1, 1)]
+        possible_positions = self.possible_moves_general(from_row, from_col, directions)
+        return (to_row, to_col) in possible_positions
+
    
 
 
