5087c16aee63e7ad5570651069c1e97a56874c8f
RocioPortal <p.portal@alumno.um.edu.ar>
2024-09-20 20:40:17 -0300

    movimientos de la torre adaptados

--- chess/rook.py
+++ chess/rook.py
@@ -5,19 +5,10 @@ class Rook(Piece):
     white_str = "♜"
     black_str = "♖"
 
-    def valid_positions(
-        self,
-        from_row,
-        from_col,
-        to_row,
-        to_col,
-    ):
-        possible_positions = (
-            self.possible_positions_vd(from_row, from_col) +
-            self.possible_positions_va(from_row, from_col) +
-            self.possible_positions_hl(from_row, from_col) + 
-            self.possible_positions_hr(from_row, from_col)
-        )
+    def valid_positions(self, from_row, from_col, to_row, to_col):
+        # Movimientos ortogonales: arriba, abajo, izquierda, derecha
+        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+        possible_positions = self.possible_moves_general(from_row, from_col, directions)
         return (to_row, to_col) in possible_positions
 
 
