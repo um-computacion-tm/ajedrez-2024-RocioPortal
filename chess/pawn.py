68401c772633b62e3c84b4383f472b388f029f8e
RocioPortal <p.portal@alumno.um.edu.ar>
2024-09-23 12:03:20 -0300

    .

--- chess/pawn.py
+++ chess/pawn.py
@@ -56,30 +56,3 @@ class Pawn(Piece):
                 else:
                     return []
         return []
-
-
-    '''
-    def is_valid_move(self, board, from_row, from_col, to_row, to_col):
-        # Movimiento vertical básico
-        direction = 1 if self.color == "WHITE" else -1
-        start_row = 6 if self.color == "WHITE" else 1
-
-        # Movimiento de un solo paso hacia adelante
-        if from_col == to_col:
-            if (to_row - from_row) == direction and board.get_piece(to_row, to_col) is None:
-                return True
-            # Movimiento de dos pasos desde la posición inicial
-            if (from_row == start_row and (to_row - from_row) == 2 * direction and
-                    board.get_piece(to_row, to_col) is None and
-                    board.get_piece(from_row + direction, to_col) is None):
-                return True
-            
-        # Movimiento en diagonal
-        if abs(from_col - to_col) == 1 and (to_row - from_row) == direction:
-            target_piece = board.get_piece(to_row, to_col)
-            if target_piece is not None and target_piece.color != self.color:
-                return True
-        
-        return False
-
-    '''
