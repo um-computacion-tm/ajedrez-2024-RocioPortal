b5bc45c495fa1e326494c9e2bdfc34a7652d0dde
RocioPortal <p.portal@alumno.um.edu.ar>
2024-10-04 23:59:45 -0300

    funcion de empate

--- chess/chess.py
+++ chess/chess.py
@@ -28,6 +28,20 @@ class Chess:
         self.__board__.move(from_row, from_col, to_row, to_col)
         self.change_turn()
 
+
+    # Usuario elige terminan o no la partida (ofrece empate)
+
+    def offer_draw(self):
+        import sys
+        print("¿Quiere terminar la partida? (si/no)")
+        user_input = input().strip().lower()
+        if user_input == "si":
+            print("Su partida ha sido terminada, gracias por jugar!")
+            return self.finish()
+        else:
+            print("Su partida continúa.")
+            return True
+        
     @property
     def turn(self):
         return self.__turn__
